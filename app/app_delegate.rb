5c446fb94955a9fd21784bdcf06d9b3cb4c1974f
Nader Hendawi <nader.hendawi@axial.net>
2014-06-27 14:07:23 -0400

    registering working
    Signed-off-by: Nader Hendawi <nader.hendawi@axial.net>

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,7 @@ class AppDelegate
   attr_reader :window
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    UIApplication.sharedApplication.registerForRemoteNotificationTypes(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
     main_controller = MainController.new
@@ -9,5 +10,13 @@ class AppDelegate
 
     @window.makeKeyAndVisible
     true
+  end
+
+  def application(application, didRegisterForRemoteNotificationsWithDeviceToken: device_token)
+    NSLog("My token is: %@", device_token)
+  end
+
+  def application(application, didFailToRegisterForRemoteNotificationsWithError: error)
+    NSLog(error.inspect)
   end
 end
