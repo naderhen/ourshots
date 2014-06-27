21cf6dc3b2862327fb6db7389423ee21e2f1d03f
Nader Hendawi <nader.hendawi@axial.net>
2014-06-27 15:09:17 -0400

    test push script
    Signed-off-by: Nader Hendawi <nader.hendawi@axial.net>

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 ourshots
 ===================
+
+require 'houston'
+require 'pry'
+
+# Environment variables are automatically read, or can be overridden by any specified options. You can also
+# conveniently use `Houston::Client.development` or `Houston::Client.production`.
+APN = Houston::Client.development
+APN.certificate = File.read("/Users/nhendawi/Dropbox/ios_certs/apple_push_notification.pem")
+
+# An example of the token sent back when a device registers for notifications
+token = "<934d08a6 307d54eb 78ca8ad9 6a0c6baf 57c2e51f c66e8504 f48fc2b5 42986a67>"
+# token = "<ce8be627 2e43e855 16033e24 b4c28922 0eeda487 9c477160 b2545e95 b68b5969>"
+
+# Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
+notification = Houston::Notification.new(device: token)
+notification.alert = "NADER RULES!"
+
+# Notifications can also change the badge count, have a custom sound, indicate available Newsstand content, or pass along arbitrary data.
+notification.badge = 57
+notification.sound = "sosumi.aiff"
+notification.content_available = true
+notification.custom_data = {foo: "bar"}
+
+APN.push(notification)
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,6 +5,8 @@ class AppDelegate
     UIApplication.sharedApplication.registerForRemoteNotificationTypes(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
+    UIApplication.sharedApplication.applicationIconBadgeNumber = 0
+
     main_controller = MainController.new
     @window.rootViewController = UINavigationController.alloc.initWithRootViewController(main_controller)
 
@@ -12,11 +14,22 @@ class AppDelegate
     true
   end
 
-  def application(application, didRegisterForRemoteNotificationsWithDeviceToken: device_token)
-    NSLog("My token is: %@", device_token)
+  def application(application, didRegisterForRemoteNotificationsWithDeviceToken:deviceToken)
+    clean_token = deviceToken.description.gsub(" ", "").gsub("<", "").gsub(">", "")
+    NSLog("TOKEN %@", clean_token)
+    # 934d08a6307d54eb78ca8ad96a0c6baf57c2e51fc66e8504f48fc2b542986a67
+    # show_alert("Push Notification Token", clean_token)
   end
-
-  def application(application, didFailToRegisterForRemoteNotificationsWithError: error)
-    NSLog(error.inspect)
+  
+  def application(application, didReceiveRemoteNotification:userInfo)
+    # show_alert("Push Notification Received", "Alert: #{userInfo['aps']}")
+    NSLog("RECEIVED!!!!!!!!! %@", userInfo)
+  end
+  
+  def show_alert(title, message)
+    alert = UIAlertView.new
+    alert.title = title
+    alert.message = message
+    alert.show
   end
 end
